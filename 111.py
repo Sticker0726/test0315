11de7df5358d8e78525006070a9119309c9b80c3
sticker <286933867@qq.com>
2023-03-13 23:01:22 +0800

    update common

--- 111.py
+++ 111.py
@@ -6,8 +6,10 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.automap import automap_base
 from multiprocessing import Pool
 
+
 class DbMgr():
     ''' 连接数据库，从数据库获取 用户表，文章表数据 '''
+
     def __init__(self):
         eng = create_engine('mysql+pymysql://root:1@localhost/test?charset=utf8')
         # 使用 automap 自动反射出 member，article 表对应的类
@@ -20,6 +22,7 @@ class DbMgr():
         # 获取操作数据库的
         Session = sessionmaker(eng, autocommit=True)
         self._ses = Session()
+
     def get_data(self):
         ''' 查询用户表，文章表
         '''
@@ -27,24 +30,31 @@ class DbMgr():
         self._articles = self._ses.query(self._Article).all()
         self._articles = [('ar', i.arid, i.title, i.tags, i.description) for i in self._articles]
         return list(self._users) + list(self._articles)
+
     class Searcher():
         ''' 进城处理函数，查找符合条件的结果，找到后返回结果 '''
+
         def __init__(self, keyword):
             self._keyword = keyword
+
         def run(self, data):
             ''' 查找字符串 '''
             try:
                 if self._keyword in str(data):
                     return 'ret: ' + str(os.getpid()) + '->' + str(data)
-                else: return None
+                else:
+                    return None
             except Exception as e:
                 return e
+
         def callback(self, data):
             ''' 全部执行完后回调函数，展示结果 '''
             try:
                 for i in data:
                     if i: print('match: {}'.format(i))
-            except Exception as e: print(e)
+            except Exception as e:
+                print(e)
+
         def main():
             # 从数据库读取数据
             mgr = DbMgr()
